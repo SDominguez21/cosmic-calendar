b8bcfdb437b7f1b093e7a0a567a2738371f0ec75
Serena Dominguez <serenadominguez@gmail.com>
2019-07-30 21:23:06 -0400

    more seeds

--- models/CosmicEvent.js
+++ models/CosmicEvent.js
@@ -3,7 +3,9 @@ const Schema = mongoose.Schema;
 
 const cosmiceventSchema = new Schema({
   name: String,
-  date: [String],
+  date: [String], //check with moment.js documentation date/period if meteor 
+  // peak: [String], BASED ON MIAMI 
+  //meeteor shower having parent comet--parent: String
   location: String,
   description: String,
   img: String
@@ -12,3 +14,5 @@ const cosmiceventSchema = new Schema({
 const CosmicEvent = mongoose.model("CosmicEvent", cosmiceventSchema);
 
 module.exports = CosmicEvent;
+
+// SHOULD I HAVE A DIFFERENT MODEL FOR EACH DIFFERENT KIND OF EVENT? i.e Full moon, meteor shower, solstice, ect.
